dbf3f5e645e80e732a1b6ff5c52ab7292ccb94fb
Samuel Barrera <sbarrera@gmail.com>
2018-12-11 20:05:51 -0300

    Actualizacion

--- src/main/java/cl/ciisa/negocio/Curso.java
+++ src/main/java/cl/ciisa/negocio/Curso.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cl.ciisa.negocio;
 
-/**
- *
- * @author Samuel Barrera
- */
+import cl.ciisa.data.DBCurso;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.core.Response;
+
 public class Curso {
+    public static final String DBCURSO = "DBCurso";
+
+    private enum MensajeError{
+        listaCursos("Error al listar los curso - "),
+        buscarCursoPorId("Error, no se encontro el curso - "),
+        guardarCurso("Error al guardar el curso - "),
+        actualizaCurso("Error al actualizar el curso - "),
+        eliminaCurso("Error al eliminar el curso - ");
+
+        private String mensaje;
+
+        MensajeError(String mensaje){
+            this.mensaje = mensaje;
+        }
+
+        public String getMensaje() {
+            return this.mensaje;
+        }
+    }
     
+    public Response listar(){
+        Response retorno = null;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(DBCURSO);
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            List<DBCurso> lista = em.createNamedQuery(DBCURSO + ".findAll").getResultList();
+            if (lista.size() <= 0) {
+                retorno = Response.noContent().build();
+            } else {
+                retorno = Response.ok().entity(lista).build();
+            }
+        } catch (Exception e) {
+            retorno = Response.serverError().entity(MensajeError.listaCursos.getMensaje() + e.getMessage()).build();
+        } finally {
+            em.close();
+        }
+        return retorno;
+    }
 }
